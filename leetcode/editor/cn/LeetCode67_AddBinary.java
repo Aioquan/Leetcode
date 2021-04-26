d2266d54ee6702db7ec7a773201668867016f12d
aioquan <aioquan@163.com>
2021-04-26 21:42:56 +0800

    616792

---
+++ leetcode/editor/cn/LeetCode67_AddBinary.java
@@ -0,0 +1,58 @@
+//给你两个二进制字符串，返回它们的和（用二进制表示）。 
+//
+// 输入为 非空 字符串且只包含数字 1 和 0。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: a = "11", b = "1"
+//输出: "100" 
+//
+// 示例 2: 
+//
+// 输入: a = "1010", b = "1011"
+//输出: "10101" 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 每个字符串仅由字符 '0' 或 '1' 组成。 
+// 1 <= a.length, b.length <= 10^4 
+// 字符串如果不是 "0" ，就都不含前导零。 
+// 
+// Related Topics 数学 字符串 
+// 👍 602 👎 0
+
+package leetcode.editor.cn;
+
+public class LeetCode67_AddBinary {
+    public static void main(String[] args) {
+        Solution solution = new LeetCode67_AddBinary().new Solution();
+        System.out.println(solution.addBinary("1010", "1011"));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String addBinary(String a, String b) {
+            StringBuilder sb = new StringBuilder();
+            int ca = 0;
+            int sum = 0;
+            for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+                sum = ca;
+                sum += i >= 0 ? a.charAt(i) - '0' : 0;
+                sum += j >= 0 ? b.charAt(j) - '0' : 0;
+                sb.append(sum % 2);
+                ca = sum / 2;
+            }
+
+            sb.append(ca == 1 ? ca : "");
+
+            return sb.reverse().toString();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ leetcode/editor/cn/LeetCode92_ReverseLinkedListIi.java
@@ -0,0 +1,100 @@
+//给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链
+//表节点，返回 反转后的链表 。
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], left = 2, right = 4
+//输出：[1,4,3,2,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [5], left = 1, right = 1
+//输出：[5]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目为 n 
+// 1 <= n <= 500 
+// -500 <= Node.val <= 500 
+// 1 <= left <= right <= n 
+// 
+//
+// 
+//
+// 进阶： 你可以使用一趟扫描完成反转吗？ 
+// Related Topics 链表 
+// 👍 880 👎 0
+
+package leetcode.editor.cn;
+
+public class LeetCode92_ReverseLinkedListIi {
+    public static void main(String[] args) {
+        Solution solution = new LeetCode92_ReverseLinkedListIi().new Solution();
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+        System.out.println(solution.reverseBetween(listNode, 2, 4));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseBetween(ListNode head, int left, int right) {
+            ListNode res = new ListNode(-1);
+            res.next = head;
+
+            ListNode guard = res;
+            for (int i = 0; i < left - 1; i++) {
+                guard = guard.next;
+            }
+            ListNode pre = guard.next;
+
+            for (int i = 0; i < right - left; i++) {
+                ListNode remove = pre.next;
+                pre.next = pre.next.next;
+
+                remove.next = guard.next;
+                guard.next = remove;
+            }
+
+            return res.next;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
